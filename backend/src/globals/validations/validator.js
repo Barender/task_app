208648846804003e1c58dc71148953fc9d86bdf3
Barender Singh <gagan007bangs@gmail.com>
2022-12-06 19:09:39 +0530

    small bug fixed

--- backend/src/globals/validations/validator.js
+++ backend/src/globals/validations/validator.js
@@ -3,7 +3,7 @@ const httpStatus = require('http-status');
 const validate = (schema) => (req, res, next) => {
   const { error } = schema.validate(req.body);
   if (error) {
-    return res.status(httpStatus.BAD_REQUEST).json({ success: false, error: error.details[0].message });
+    return res.status(httpStatus.BAD_REQUEST).json({ success: false, message: error.details[0].message });
   } else {
     next();
   }
